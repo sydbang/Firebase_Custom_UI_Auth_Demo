a990bde30c116666f47f70cd720dbb115f7c3250
sydbang <sydney.bang94@gmail.com>
2022-03-17 19:54:44 +0900

    Initial Commit

---
+++ Firebase_Custom_UI_Auth_Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Firebase_Custom_UI_Auth_Demo
+//
+//  Created by Sunghee Bang on 2022-03-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Firebase_Custom_UI_Auth_Demo/Firebase_Custom_UI_Auth_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_Custom_UI_Auth_DemoApp.swift
+//  Firebase_Custom_UI_Auth_Demo
+//
+//  Created by Sunghee Bang on 2022-03-17.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_Custom_UI_Auth_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
